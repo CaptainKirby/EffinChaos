a59d2bfbc8da51c649c0b734f6634215b7dc40d3
SpaceTest <Jebruhal@gmail.com>
2014-01-10 22:57:27 +0100

    effing crap

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,11 +10,15 @@ public class Movement : MonoBehaviour {
 	public float movementMax = 20f;
 	private bool rotated = false;
 	public float mTime;
-	public float mTime2;
 	public Vector3 rot;
-	public bool rotatingBack;
-	public float rotZ;
-	private float curVel;
+	public bool pressedTrigger;
+	public float rotTo;
+	public float rotationDur = 7f;
+	public bool isRunning;
+	public Quaternion startRot;
+	public bool sloppyBool;
+	private bool pressedDown;
+	private bool pressedUp;
 	void Start () {
 	
 	}
@@ -23,16 +27,74 @@ public class Movement : MonoBehaviour {
 		triggers = Input.GetAxis ("Triggers");
 		inputDir.x = Input.GetAxis ("Horizontal"); 
 		inputDir.y = Input.GetAxis ("Vertical");
-		if (triggers == -1 && !rotated)
+
+		if (!isRunning) 
 		{
-			Rotate (this.transform.rotation.z, 90);
+			startRot = this.transform.rotation;
+//			mTime = 0;
+			if(!pressedTrigger)
+			{
+				sloppyBool = true;
+			}
+		}
+		if (pressedTrigger) 
+		{
+			if(!pressedDown)
+			{
+				pressedDown = true;
+			}
+			if(sloppyBool)
+			{
+				mTime = 0;
+				sloppyBool = false;
+			}
+//			StartCoroutine("Rotate", rotTo);
+			Quaternion rot;
+			Quaternion newRot = Quaternion.Euler (this.transform.rotation.x, this.transform.rotation.y, rotTo);
+//			startRot = this.transform.rotation;
+			//		while (true)
+			//		{
+			if(mTime < 1)
+			{
+				isRunning = true;
+				mTime += Time.deltaTime / rotationDur;
+				rot = Quaternion.Lerp(startRot,newRot, mTime);
+				this.transform.rotation = rot;
+				//				mTime += Time.deltaTime;
+				//				float curRotZ = transform.rotation
+				//				rot.z = Mathf.SmoothDamp(this.transform.rotation,to, ref curVel,1);
+				//				this.transform.eulerAngles = rot;
+			}
+			else{ isRunning = false;}
+		}
+
+
+//		else StartCoroutine("Rotate", rotTo);
+
+		if (triggers == -1)
+		{
+			pressedTrigger = true;
+			rotTo = -90;
+//			StartCoroutine("Rotate", 90);
 			//rotter til 90 i z
 		}
-		if (triggers == 1 && !rotated)
+		if (triggers == 1) 
 		{
-			Rotate (this.transform.rotation.z, -90);
-			//rotter til 90 i z
+			pressedTrigger = true;
+			rotTo = 90;
 		}
+		if ((int)triggers == 0) 
+		{
+			pressedTrigger = false;
+			rotTo = 0;
+//			mTime = 0;
+
+		}
+//		if (triggers == 1 && !rotated)
+//		{
+//			Rotate (this.transform.rotation.z, -90);
+//			//rotter til 90 i z
+//		}
 
 //		else if (triggers == 1)
 //		{
@@ -52,21 +114,47 @@ public class Movement : MonoBehaviour {
 		rigidbody.velocity = inputDir.normalized * speed;
 	}
 
-	void Rotate(float from, float to)
-	{
-		if (mTime < 1)
-		{
-			mTime += Time.deltaTime;
-			rot.z = Mathf.SmoothStep(from,to,mTime);
-			this.transform.eulerAngles = rot;
-//			
-		}
-		else
-		{
-			mTime = 0;
-			rotated = true;
-		}
-	}
+//	IEnumerator Rotate(float to)
+//	{
+//		float newRot = Mathf.SmoothDamp (transform.rotation.z, to, ref curVel, 1);
+//		test = Mathf.DeltaAngle(,to);
+//		rot.z = newRot;
+//		yield return null;
+//		mTime = 0;
+//		float prevZ = this.transform.rotation.z;
+
+//		Quaternion rot;
+//		Quaternion newRot = Quaternion.Euler (this.transform.rotation.x, this.transform.rotation.y, to);
+//		Quaternion startRot = this.transform.rotation;
+////		while (true)
+////		{
+//			if(mTime < 1)
+//			{
+//				mTime += Time.deltaTime / rotationDur;
+//				rot = Quaternion.Lerp(startRot,newRot, mTime);
+//				this.transform.rotation = rot;
+////				mTime += Time.deltaTime;
+////				float curRotZ = transform.rotation
+////				rot.z = Mathf.SmoothDamp(this.transform.rotation,to, ref curVel,1);
+////				this.transform.eulerAngles = rot;
+//			}
+//		else mTime = 0;
+//
+//			yield return null;
+//		}
+//		if (mTime < 1)
+//		{
+//			mTime += Time.deltaTime;
+//			rot.z = Mathf.SmoothStep(from,to,mTime);
+//			this.transform.eulerAngles = rot;
+////			
+//		}
+//		else
+//		{
+//			mTime = 0;
+//			rotated = true;
+//		}
+//	}
 
 }
 
