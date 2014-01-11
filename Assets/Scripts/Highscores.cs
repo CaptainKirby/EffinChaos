24467702a8ae992f2c77635329a03dcd1328f44e
CaptainKirby <s.engelbrecht@gmail.com>
2014-01-11 23:56:18 +0100

    highscore + spaceship prefab

--- Assets/Scripts/Highscores.cs
+++ Assets/Scripts/Highscores.cs
@@ -1,41 +1,104 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+
 using System.Linq;
+
 public class Highscores : MonoBehaviour {
-	public List<int> unsortedHighscoreScoresList;
-	public List<int> sortedHighscoreScoresList;
-	public int[] highscoreScores = new int[10];
+	public List<KeyValuePair<int,string>> highscoreListTest = new List<KeyValuePair<int, string>>();
+//	public List<int> unsortedHighscoreScoresList;
+//	public List<int> sortedHighscoreScoresList;
+	public List<int> highscoreScoresList;
+	public List<string> highscoreNamesList;
+	public int[] highscoreScores;
+	public string[] highscoreNames;
 	public bool setHighscore;
 	public bool getHighscore;
 	public int score;
+	public float scoreCountSpeed = 3;
+	private float scoreFloat;
 	public string name;
 	public bool dead;
+	private bool pauseCount;
+	public bool deletePrefs;
 	void Start () {
-		highscoreScores = PlayerPrefsX.GetIntArray("Scores");
-		unsortedHighscoreScoresList = highscoreScores.ToList();
+		pauseCount = false;
+//		highscoreScores = PlayerPrefsX.GetIntArray("Scores");
+		highscoreScores = PlayerPrefsX.GetIntArray("HighscoreScores");
+		highscoreNames = PlayerPrefsX.GetStringArray("HighscoreNames");
+		highscoreScoresList = highscoreScores.ToList();
+		highscoreNamesList = highscoreNames.ToList();
+//		foreach(int hsS in highscoreScoresList)
+//		{
+//			foreach(string hsN in highscoreNamesList)
+//			{
+		for(int i = 0; i< highscoreNamesList.Count; i++)
+		{
+				highscoreListTest.Add(new KeyValuePair<int, string>(highscoreScoresList[i], highscoreNamesList[i]));
+		}
+//			}
+//		}
+//		unsortedHighscoreScoresList = highscoreScores.ToList();
 	}
 	
 	void Update () {
-		if(setHighscore)
+		if(deletePrefs)
 		{
+			PlayerPrefs.DeleteAll();
+		}
+		if(!pauseCount)
+		{
+			scoreFloat += Time.deltaTime * scoreCountSpeed;
+			score = (int)scoreFloat;
+		}
+		if(dead)
+		{
+			setHighscore = true;
+		}
+		if(setHighscore && dead)
+		{
+			pauseCount = true;
+			dead = false;
 			setHighscore = false;
 			SetScores(name, score);
+			scoreFloat = 0;
 		}
 		if(getHighscore)
 		{
 			getHighscore = false;
-			Debug.Log (highscoreScores.Length);
+//			Debug.Log (highscoreScores.Length);
 			
 		}
 		
 	}
 	void SetScores(string name, int score)
 	{
-		unsortedHighscoreScoresList.Add(score);
-		sortedHighscoreScoresList = unsortedHighscoreScoresList.OrderBy(i => i).ToList();
-		highscoreScores = sortedHighscoreScoresList.ToArray(); 
-		PlayerPrefsX.SetIntArray ("Scores", highscoreScores);
+		highscoreListTest.Add(new KeyValuePair<int, string>(score, name));
+		highscoreListTest.Sort((KeyValuePair<int, string> hslt, KeyValuePair<int, string> hslt2) =>
+		{
+			return hslt.Key.CompareTo(hslt2.Key);
+		});
+		highscoreScoresList.RemoveRange(0, highscoreScoresList.Count);
+		highscoreNamesList.RemoveRange(0, highscoreNamesList.Count);
+		foreach (KeyValuePair<int, string> hslt in highscoreListTest)
+		{
+
+				highscoreScoresList.Add(hslt.Key);
+				highscoreNamesList.Add(hslt.Value);
+
+			Debug.Log(hslt.Value + ":" +hslt.Key);
+		}
+
+//		unsortedHighscoreScoresList.Add(score);
+
+//		sortedHighscoreScoresList = unsortedHighscoreScoresList.OrderBy(i => i).ToList();
+//		highscoreListTest.CopyTo(highscoreScores,0);
+//		highscoreScores = highscoreListTest
+//		highscoreScores.
+		highscoreScores = highscoreScoresList.ToArray(); 
+		highscoreNames = highscoreNamesList.ToArray();
+		PlayerPrefsX.SetIntArray ("HighscoreScores", highscoreScores);
+		PlayerPrefsX.SetStringArray ("HighscoreNames", highscoreNames);
 	}
 	void AddScore(string name, int score){
 		
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+public class MainMenu : MonoBehaviour {
+	public List<int> unsortedHighscoreScoresList;
+	public List<int> sortedHighscoreScoresList;
+	public int[] highscoreScores = new int[10];
+	public string[] highscoreNames = new string[10];
+	void Start () {
+		highscoreScores = PlayerPrefsX.GetIntArray("HighscoreScores");
+		highscoreNames = PlayerPrefsX.GetStringArray("HighscoreNames");
+//		unsortedHighscoreScoresList = highscoreScores.ToList();
+//		sortedHighscoreScoresList = unsortedHighscoreScoresList.OrderBy(i => i).ToList();
+//		highscoreScores = sortedHighscoreScoresList.ToArray(); 
+	}
+	
+
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+
+	}
+}
